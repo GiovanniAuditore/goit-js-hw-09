f0bea1dd95c3a1229ba4f0ba839e4a2681131d4d
GiovanniAuditore <66073653+GiovanniAuditore@users.noreply.github.com>
2025-05-24 11:40:26 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,13 +1,11 @@
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
 
-// Начальное значение объекта с данными формы
 let formData = {
   email: '',
   message: '',
 };
 
-// 1. Проверка: если в localStorage уже есть сохранённые данные — восстановить их
 const savedData = localStorage.getItem(STORAGE_KEY);
 if (savedData) {
   formData = JSON.parse(savedData);
@@ -15,13 +13,12 @@ if (savedData) {
   form.message.value = formData.message || '';
 }
 
-// 2. Обработчик ввода — обновляем formData и сохраняем в localStorage
+
 form.addEventListener('input', e => {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 });
 
-// 3. Обработчик отправки формы
 form.addEventListener('submit', e => {
   e.preventDefault();
 
@@ -30,9 +27,7 @@ form.addEventListener('submit', e => {
     return;
   }
 
-  console.log('Отправка данных:', formData);
 
-  // Очистка
   localStorage.removeItem(STORAGE_KEY);
   form.reset();
   formData = { email: '', message: '' };
